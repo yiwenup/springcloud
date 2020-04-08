252ba7a111c94851bb210760403f0246c00837cb
Giraffe <97035457@qq.com>
2020-04-08 16:43:11 +0800

    Hystrix Dashboard搭建

--- cloud-provider-hystrix-payment8005/src/main/java/com/ncu/springcloud/PaymentHystrixMain8005.java
+++ cloud-provider-hystrix-payment8005/src/main/java/com/ncu/springcloud/PaymentHystrixMain8005.java
@@ -1,9 +1,12 @@
 package com.ncu.springcloud;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -11,5 +14,18 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 public class PaymentHystrixMain8005 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentHystrixMain8005.class, args);
+    }
+
+    /*
+    * 该配置为了服务监控而配置，与服务容器本身无关，是spring cloud升级后的坑
+    * */
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
     }
 }
